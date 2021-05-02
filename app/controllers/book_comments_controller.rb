72ca29143d957e50825ecfa5ed9c4856eb69737a
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-05-02 04:54:39 +0000

    [finish]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -9,7 +9,7 @@ class BookCommentsController < ApplicationController
 
   def destroy
     # binding.pry
-    BookComment.find_by(params[:id]).destroy
+    BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
     redirect_to request.referer
   end
   
